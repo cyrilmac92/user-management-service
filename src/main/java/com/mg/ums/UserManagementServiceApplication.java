6c271a2de118330926c3d0db8acbc6172030374a
cyril mac philip <cyrilmac92@yahoo.com>
2016-09-02 00:24:47 +0530

    intitial project set up.

---
+++ src/main/java/com/mg/ums/UserManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mg.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementServiceApplication.class, args);
+	}
+}
